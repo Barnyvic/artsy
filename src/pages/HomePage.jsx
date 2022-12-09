bdffd6bdabd9846a579b84d350d3fa3e15456cf4
Barny Victor <victorbarny4@gmail.com>
2022-12-09 17:38:11 +0100

    added more images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import SliderComponet from "../components/navbar/sliderComponet";
 import { motion } from "framer-motion";
+import featureIMg1 from "../images/egypt.png";
+import featureIMg2 from "../images/Rectangle 241.png";
+import featureIMg3 from "../images/Rectangle 29.png";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 function HomePage() {
   return (
@@ -29,6 +33,23 @@ function HomePage() {
           <main className="Home-cotainer">
             <div className="Home-container-body">
               <h2>Featured products</h2>
+              <div className="feature-Content-container">
+                {featureContents.map((items) => (
+                  <div key={items.id}>
+                    <img src={items.img} alt="images" />
+                    <div className="feature-contents">
+                      <h2>{items.header}</h2>
+                      <p>{items.para}</p>
+                      <div className="feature-contents-footer">
+                        <p>{items.footer}</p>
+                        <button>
+                          <BsArrowRightCircle />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </main>
         </div>
@@ -38,3 +59,27 @@ function HomePage() {
 }
 
 export default HomePage;
+
+const featureContents = [
+  {
+    id: 1,
+    img: featureIMg2,
+    header: "The Boolean Egyptian",
+    para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+    footer: "64 major creators",
+  },
+  // {
+  //   id: 2,
+  //   img: featureIMg3,
+  //   header: "The Boolean Egyptian",
+  //   para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+  //   footer: "64 major creators",
+  // },
+  // {
+  //   id: 3,
+  //   img: featureIMg1,
+  //   header: "The Boolean Egyptian",
+  //   para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor pur",
+  //   footer: "64 major creators",
+  // },
+];
